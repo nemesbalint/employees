8ffbede71989f7c2191b26d7873aacd127bcafda
Alerant_NemesB <balint.nemes@erstebank.hu>
2023-05-14 20:50:50 +0200

    REST webszolgáltatások POST és DELETE művelet - gyakorlat - Create, Update és delete

--- src/main/java/employees/EmployeeService.java
+++ src/main/java/employees/EmployeeService.java
@@ -9,6 +9,7 @@ import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 import java.util.Optional;
+import java.util.concurrent.atomic.AtomicLong;
 import java.util.stream.Collectors;
 
 @Service
@@ -16,10 +17,13 @@ public class EmployeeService {
 
     private ModelMapper modelMapper;
 
+
+    private AtomicLong id = new AtomicLong();
+
     private List<Employee> employees = Collections.synchronizedList(new ArrayList<>(
             List.of(
-                    new Employee(1L, "John Doe"),
-                    new Employee(2L, "Jack Doe")
+                    new Employee(id.getAndIncrement(), "John Doe"),
+                    new Employee(id.getAndIncrement(), "Jack Doe")
             )
     ));
 
@@ -42,5 +46,28 @@ public class EmployeeService {
                 .filter(e -> e.getId() == id).findAny()
                 .orElseThrow(() -> new IllegalArgumentException("Employee not foud: "+id)),
                 EmployeeDto.class);
+    }
+
+    public EmployeeDto createEmployee(CreateEmployeeCommand command) {
+        Employee employee = new Employee(id.getAndIncrement(), command.getName());
+        employees.add(employee);
+        return modelMapper.map(employee, EmployeeDto.class);
+    }
+
+    public EmployeeDto updateEmployee(long id, UpdateEmployeeCommand command) {
+        Employee employee = employees.stream()
+                .filter(e-> e.getId() == id)
+                .findFirst()
+                .orElseThrow(()-> new IllegalArgumentException("Employee not found: "+id));
+        employee.setName(command.getName());
+        return modelMapper.map(employee, EmployeeDto.class);
+    }
+
+    public void deleteEmployee(long id) {
+        Employee employee = employees.stream()
+                .filter(e-> e.getId() == id)
+                .findFirst()
+                .orElseThrow(()-> new IllegalArgumentException("Employee not found: "+id));
+        employees.remove(employee);
     }
 }
--- src/main/java/employees/EmployeesController.java
+++ src/main/java/employees/EmployeesController.java
@@ -1,7 +1,11 @@
 package employees;
 
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -25,5 +29,16 @@ public class EmployeesController {
     public EmployeeDto findEmployeeById(@PathVariable("id") long id) {
         return employeeService.findEmployeeById(id);
     }
-
+    @PostMapping
+    public EmployeeDto createEmployee(@RequestBody CreateEmployeeCommand command) {
+        return employeeService.createEmployee(command);
+    }
+    @PutMapping("/{id}")
+    public EmployeeDto updateEmployee(@PathVariable("id") long id, @RequestBody UpdateEmployeeCommand command) {
+        return employeeService.updateEmployee(id, command);
+    }
+    @DeleteMapping("/{id}")
+    public void deleteEmployee(@PathVariable("id") long id) {
+        employeeService.deleteEmployee(id);
+    }
 }
