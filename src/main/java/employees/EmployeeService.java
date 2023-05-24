223c5afee3cd143b9832f9f93c4fbe823dd56692
Alerant_NemesB <balint.nemes@erstebank.hu>
2023-05-24 21:42:31 +0200

    Kommentek eltávolítása

--- src/main/java/employees/EmployeeService.java
+++ src/main/java/employees/EmployeeService.java
@@ -1,10 +1,6 @@
 package employees;
 
-//import org.modelmapper.ModelMapper;
-//import org.modelmapper.TypeToken;
 import org.springframework.stereotype.Service;
-
-import java.lang.reflect.Type;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
@@ -14,8 +10,6 @@ import java.util.stream.Collectors;
 
 @Service
 public class EmployeeService {
-
-//    private ModelMapper modelMapper;
 
     private EmployeeMapper employeeMapper;
 
@@ -28,33 +22,20 @@ public class EmployeeService {
             )
     ));
 
-//    public EmployeeService(ModelMapper modelMapper) {
-//        this.modelMapper = modelMapper;
-//    }
-
     public EmployeeService(EmployeeMapper employeeMapper) {
         this.employeeMapper = employeeMapper;
     }
 
     public List<EmployeeDto> listEmployees(Optional<String> prefix) {
-//        Type targetTypeList = new TypeToken<List<Employee>>(){}.getType();
 
         List<Employee> filtered = employees.stream()
                 .filter(e -> prefix.isEmpty() || e.getName().toLowerCase().startsWith(prefix.get().toLowerCase()))
                 .collect(Collectors.toList());
 
-//        return modelMapper.map(filtered, targetTypeList);
         return employeeMapper.toDto(filtered);
     }
 
     public EmployeeDto findEmployeeById(long id) {
-//        return modelMapper.map(employees.stream()
-//                .filter(e -> e.getId() == id).findAny()
-//                .orElseThrow(() -> new IllegalArgumentException("Employee not foud: "+id)),
-//                EmployeeDto.class);
-//        return employeeMapper.toDto(employees.stream()
-//                        .filter(e -> e.getId() == id).findAny()
-//                        .orElseThrow(() -> new IllegalArgumentException("Employee not found: "+id)));
         return employeeMapper.toDto(employees.stream()
                 .filter(e -> e.getId() == id).findAny()
                 .orElseThrow(() -> new EmployeeNotFoundException(id)));
@@ -63,7 +44,6 @@ public class EmployeeService {
     public EmployeeDto createEmployee(CreateEmployeeCommand command) {
         Employee employee = new Employee(id.getAndIncrement(), command.getName());
         employees.add(employee);
-//        return modelMapper.map(employee, EmployeeDto.class);
         return employeeMapper.toDto(employee);
     }
 
@@ -71,10 +51,8 @@ public class EmployeeService {
         Employee employee = employees.stream()
                 .filter(e-> e.getId() == id)
                 .findFirst()
-//                .orElseThrow(()-> new IllegalArgumentException("Employee not found: "+id));
                 .orElseThrow(()-> new EmployeeNotFoundException(id));
         employee.setName(command.getName());
-//        return modelMapper.map(employee, EmployeeDto.class);
         return employeeMapper.toDto(employee);
     }
 
@@ -82,7 +60,6 @@ public class EmployeeService {
         Employee employee = employees.stream()
                 .filter(e-> e.getId() == id)
                 .findFirst()
-//                .orElseThrow(()-> new IllegalArgumentException("Employee not found: "+id));
                 .orElseThrow(()-> new EmployeeNotFoundException(id));
         employees.remove(employee);
     }
