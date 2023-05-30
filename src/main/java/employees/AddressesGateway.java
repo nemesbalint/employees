df9a396d22552db40f93a6b68af1a402c9a7a3a7
Alerant_NemesB <balint.nemes@erstebank.hu>
2023-05-30 22:15:48 +0200

    WebClient

--- src/main/java/employees/AddressesGateway.java
+++ src/main/java/employees/AddressesGateway.java
@@ -1,24 +1,20 @@
 package employees;
 
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.stereotype.Service;
-import org.springframework.web.client.RestTemplate;
+import org.springframework.web.reactive.function.client.WebClient;
 
 @Service
 @Slf4j
 public class AddressesGateway {
 
-    private final RestTemplate restTemplate;
-
-    public AddressesGateway(RestTemplateBuilder builder) {
-        restTemplate = builder.build();
-    }
-
     public AddressDto findAddressByName(String name) {
         log.debug("findAddressByName name: {}", name);
-        return restTemplate.getForObject("http://localhost:8081/api/addresses?name={name}",
-                AddressDto.class, name);
+        return WebClient.create("http://localhost:8081")
+                .get()
+                .uri(uriBuilder -> uriBuilder.path("/api/addresses").queryParam("name", name).build())
+                .retrieve()
+                .bodyToMono(AddressDto.class)
+                .block();
     }
-
 }
